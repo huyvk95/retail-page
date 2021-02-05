f4919d3b7d1b3ede9c28ab4e12aa07bfc9ff1093
huyvk95 <huyvk95@gmail.com>
2021-02-05 13:51:34 +0700

    Update

---
+++ precache-manifest.ebc0bbb6d6593d76a1f520412c02f9bb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c9ff790c76c4cede9e5101613f5309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61d94bbc8be87e4ea6d",
+    "url": "/static/css/main.b6e1885c.chunk.css"
+  },
+  {
+    "revision": "3269d4a55e5f46a181c0",
+    "url": "/static/js/2.5f47b398.chunk.js"
+  },
+  {
+    "revision": "973f511867bc028050003b876b659788",
+    "url": "/static/js/2.5f47b398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61d94bbc8be87e4ea6d",
+    "url": "/static/js/main.26661072.chunk.js"
+  },
+  {
+    "revision": "93e45c43e3051e8fb377",
+    "url": "/static/js/runtime-main.63edf607.js"
+  },
+  {
+    "revision": "8115952731fcfd8caaed63de7f9cf1bc",
+    "url": "/static/media/UTMAvoBold.81159527.eot"
+  },
+  {
+    "revision": "b53359c87c73ad1debd34228c0763762",
+    "url": "/static/media/UTMAvoBold.b53359c8.svg"
+  },
+  {
+    "revision": "f165d3d02ba5204643ada7deced64d5c",
+    "url": "/static/media/UTMAvoBold.f165d3d0.woff"
+  },
+  {
+    "revision": "fe66efea19bf2680ff126c64363962eb",
+    "url": "/static/media/UTMHelve.fe66efea.woff2"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
